6be92b18a9f478e6338599882095ec05a771c912
Jonathan Barthelemy <jbarthelemy@salesforce.com>
2017-05-24 12:17:34 -0700

    add test cookbook

---
+++ tests/cookbooks/chef-uploader-test-cookbook/README.md
@@ -0,0 +1,12 @@
+Description
+===========
+This is a cookbook automatically created by Chef Uploader for testing purposes.
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
---
+++ tests/cookbooks/chef-uploader-test-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-uploader-test-cookbook'
+maintainer       "Chef Uploader"
+maintainer_email "test@email.com"
+license          "All rights reserved"
+description      "Chef uploader release process test"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
