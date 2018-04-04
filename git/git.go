8976fb71d49400a4d37b710f4f9ae0cfaf39bfc6
Jonathan Barthelemy <jbarthelemy@salesforce.com>
2018-04-04 07:53:57 -0700

    improve err handling on git package, added more logging and return err

--- git/git.go
+++ git/git.go
@@ -12,30 +12,56 @@ import (
 var GitHubOrganizationName string
 var GitHubRepoName string
 
-func Push() {
-	helpers.RunCommand("git", "push", "origin", "master")
+func Push() error {
+	_, err := helpers.RunCommand("git", "push", "origin", "master")
+
+	if err != nil {
+		log.Errorf("Git failed to push this commit to the branch")
+		return err
+	}
+
+	log.Info("Pushed the commit to the branch")
+	return nil
 }
 
-func Commit(file string, message string) {
-	helpers.RunCommand("git", "commit", file, "-m", message)
+func Commit(file string, message string) error {
+	_, err := helpers.RunCommand("git", "commit", file, "-m", message)
+
+	if err != nil {
+		log.Errorf("Git failed to push this commit to the branch")
+		return err
+	}
 
 	log.Infof("Committed the file %s [%s]\n", file, message)
+	return nil
 }
 
-func LastCommit() string {
-	commitCmd := helpers.RunCommand("git", "log", "-n1", "--pretty=format:%h")
+func LastCommit() (string, error) {
+	var commit string
 
-	commit := string(commitCmd)
+	commitCmd, err := helpers.RunCommand("git", "log", "-n1", "--pretty=format:%h")
+
+	if err != nil {
+		log.Errorf("Git failed to get the last commit of this branch")
+		return commit, err
+	}
+
+	commit = string(commitCmd)
 	log.Infof("Last commit found : %s\n", commit)
-
-	return commit
+	return commit, nil
 }
 
-func CommitInfo(commit string) structs.CommitInfo {
+func CommitInfo(commit string) (structs.CommitInfo, error) {
 	merge := false
+	var commitInfo structs.CommitInfo
+
 	commitRegexp := regexp.MustCompile(`(?s)commit (.*)Author: (.*)Date: (.*)\n\n(.*)`)
 
-	commitCmd := helpers.RunCommand("git", "log", "-n1", commit)
+	commitCmd, err := helpers.RunCommand("git", "log", "-n1", commit)
+	if err != nil {
+		log.Errorf("Git failed to get details about the last commit of this branch")
+		return commitInfo, err
+	}
 
 	if strings.Contains(string(commitCmd), "^Merge:") {
 		commitRegexp = regexp.MustCompile(`(?s)commit (.*)Merge: .*Author: (.*)Date: (.*)\n\n(.*)`)
@@ -43,9 +69,13 @@ func CommitInfo(commit string) structs.CommitInfo {
 	}
 
 	commitSubMatch := commitRegexp.FindStringSubmatch(string(commitCmd))
-	commit, author, date, title := strings.TrimSpace(commitSubMatch[1]), strings.TrimSpace(commitSubMatch[2]), strings.TrimSpace(commitSubMatch[3]), strings.TrimSpace(commitSubMatch[4])
 
-	return structs.CommitInfo{merge, author, date, title}
+	commitInfo.IsMergeCommit = merge
+	commitInfo.Author = strings.TrimSpace(commitSubMatch[2])
+	commitInfo.Date = strings.TrimSpace(commitSubMatch[3])
+	commitInfo.Title = strings.TrimSpace(commitSubMatch[4])
+
+	return commitInfo, nil
 }
 
 func GenerateCommitURL(commit string) string {
@@ -53,13 +83,21 @@ func GenerateCommitURL(commit string) string {
 	return URL
 }
 
-func FilesListForEachCommit(commit string) []string {
-	log.Infof("Retrieving files list for the commit %s\n", commit)
-	filesCmd := helpers.RunCommand("git", "diff", "--name-only", commit, commit+"^")
+func FilesListForEachCommit(commit string) ([]string, error) {
+	var filesList []string
+	var err error
+
+	log.Infof("Retrieving files list for the commit %s", commit)
+	filesCmd, err := helpers.RunCommand("git", "diff", "--name-only", commit, commit+"^")
+	if err != nil {
+		log.Errorf("Git failed to get the list of files for commit %s", commit)
+		return filesList, err
+	}
+
 	filesCmdUnFiltered := string(filesCmd)
 	cookbookRegexp := regexp.MustCompile(`(.*cookbooks/[^/]*)/(.*)`)
 	filesCmdFiltered := cookbookRegexp.ReplaceAllString(filesCmdUnFiltered, "$1")
-	filesList := helpers.RemoveDuplicatesUnordered(strings.Split(strings.TrimSpace(filesCmdFiltered), "\n"))
+	filesList = helpers.RemoveDuplicatesUnordered(strings.Split(strings.TrimSpace(filesCmdFiltered), "\n"))
 
-	return filesList
+	return filesList, nil
 }
